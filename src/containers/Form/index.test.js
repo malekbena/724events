7c4afbf7bf6fc3c389b52c61d4b05e520cee2401
malekbena <malekb.benali@gmail.com>
2024-05-02 13:51:24 +0200

    Form test fix:  "When action is created" => add a timeout before the expect action

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Form from "./index";
 
 describe("When Events is created", () => {
@@ -23,7 +23,9 @@ describe("When Events is created", () => {
       );
 
       await screen.findByText("En cours");
-      await screen.findByText("Envoyer");
+      await waitFor(() =>
+        screen.findByText("Envoyer")
+        , { timeout: 3000 });
       expect(onSuccess).toHaveBeenCalled();
     });
   });
